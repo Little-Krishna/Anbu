ff7553c158d5a9ba8e5e110c246b8f1531483554
unknown <goki1191995@gmail.com>
2021-10-23 22:52:49 +0530

    main

---
+++ src/org/lip/Note.java
@@ -0,0 +1,23 @@
+package org.lip;
+
+public class Note {
+	
+	public void empName() {
+    System.out.println("gokul");
+		
+	}
+	
+	public void empid() {
+     System.out.println("2356776");
+	}
+	
+	public static void main(String[] args) {
+		
+		Note n = new Note();
+		
+		n.empid();
+		n.empName();
+	}
+	
+
+}
